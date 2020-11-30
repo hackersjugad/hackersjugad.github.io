410e40c71a68a8fc9de87baf2e3cadfffccc35a4
hackersjugad <hackersjugad@gmail.com>
2020-11-30 20:08:16 +0530

    add series

--- sw.js
+++ sw.js
@@ -18,53 +18,71 @@
 // servie worker
 
 
-const cacheName = 'pwa-conf-v1';
+const staticCacheName = 'pwa-conf-v1.01';
 const staticAssets = [
-    '/',
-    '/index.html',
-    '/credits.html',
-    '/404.html',
-    '/books.html',
-    '/movies.html',
-    '/series.html',
-    '/search.json',
-    '/assets/search-script.js',
-    '/assets/css/styles.css',
-    '/assets/'
+  '/',
+  '/index.html',
+  '/credits.html',
+  '/404.html',
+  '/books.html',
+  '/movies.html',
+  '/series.html',
+  '/search.json',
+  '/assets/search-script.js',
+  '/assets/css/styles.css',
+  '/assets/'
 ];
 
 
 self.addEventListener('install', async event => {
-    console.log('install event');
-    const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
+  console.log('install event');
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
 });
 
 self.addEventListener('fetch', async event => {
-    console.log('fetch event');
-    const req = event.request;
+  console.log('fetch event');
+  const req = event.request;
 
-    if (/.*(json)$/.test(req.url)) {
-        event.respondWith(networkFirst(req));
-    } else {
-        event.respondWith(cacheFirst(req));
-    }
+  if (/.*(json)$/.test(req.url)) {
+    event.respondWith(networkFirst(req));
+  } else {
+    event.respondWith(cacheFirst(req));
+  }
 });
 
 async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cachedResponse = await cache.match(req);
-    return cachedResponse || networkFirst(req);
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(req);
+  return cachedResponse || networkFirst(req);
 }
 
 async function networkFirst(req) {
-    const cache = await caches.open(cacheName);
-    try {
-        const fresh = await fetch(req);
-        cache.put(req, fresh.clone());
-        return fresh;
-    } catch (e) {
-        const cachedResponse = await cache.match(req);
-        return cachedResponse;
-    }
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cachedResponse = await cache.match(req);
+    return cachedResponse;
+  }
 }
+
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+
+  const cacheAllowlist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
