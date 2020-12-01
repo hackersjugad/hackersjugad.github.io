65d6dbd0d6fc746d6720b790abfb7c7c0052520b
hackersjugad <hackersjugad@gmail.com>
2020-12-01 21:32:16 +0530

    PWA work complete

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
-//   "shortcuts": [
-//     {
-//       "name": "All movies",
-//       "short_name": "Movies",
-//       "description": "View the list of all movies available on hacker's jugad",
-//       "url": "/movies?utm_source=homescreen",
-//       "icons": [{ "src": "/assets/movies.png", "sizes": "192x192" }]
-//     },
-//     {
-//       "name": "All web-series",
-//       "short_name": "WebSeries",
-//       "description": "View the list of all web series available on hacker's jugad",
-//       "url": "/series?utm_source=homescreen",
-//       "icons": [{ "src": "/assets/series.png", "sizes": "192x192" }]
-//     }
-//   ]
+// "shortcuts": [
+//   {
+//     "name": "All movies",
+//     "short_name": "Movies",
+//     "description": "View the list of all movies available on hacker's jugad",
+//     "url": "/movies",
+//     "icons": [{ "src": "/assets/movies.png", "sizes": "192x192" }]
+//   },
+//   {
+//     "name": "All web-series",
+//     "short_name": "WebSeries",
+//     "description": "View the list of all web series available on hacker's jugad",
+//     "url": "/series",
+//     "icons": [{ "src": "/assets/series.png", "sizes": "192x192" }]
+//   }
+// ]
 
 // servie worker
-const cacheName = 'pwa-conf-v1.28';
-const staticCacheName = [
-  '/credits.html',
-  '/404.html',
-];
+const cache_Name = "pwa-conf-v1.021";
+// const staticCacheName = [
+//   '/credits.html',
+//   '/404.html',
+// ];
 
 const staticAssets = [
   '/index.html',
@@ -34,15 +34,9 @@ const staticAssets = [
 ];
 
 
-// self.addEventListener('install', async event => {
-//   // console.log('install event');
-//   const cache = await caches.open(cacheName);
-//   await cache.addAll(staticAssets);
-// });
-
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(cache_Name).then(function (cache) {
       return cache.addAll(staticAssets);
     })
   );
@@ -60,13 +54,13 @@ self.addEventListener('fetch', async event => {
 });
 
 async function cacheFirst(req) {
-  const cache = await caches.open(cacheName);
+  const cache = await caches.open(cache_Name);
   const cachedResponse = await cache.match(req);
   return cachedResponse || networkFirst(req);
 }
 
 async function networkFirst(req) {
-  const cache = await caches.open(cacheName);
+  const cache = await caches.open(cache_Name);
   try {
     const fresh = await fetch(req);
     cache.put(req, fresh.clone());
@@ -77,20 +71,28 @@ async function networkFirst(req) {
   }
 }
 
-self.addEventListener('activate', event => {
-  console.log('Activating new service worker...');
-
-  const cacheAllowlist = [staticCacheName];
-
+self.addEventListener('activate', function (event) {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheAllowlist.indexOf(cacheName) === -1) {
+        cacheNames.filter(function (cacheName) {
+          return true;
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function (cacheName) {
+          if (cache_Name != cacheName && cacheName.startsWith("pwa-conf")) {
             return caches.delete(cacheName);
           }
         })
       );
     })
   );
+});
+
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
